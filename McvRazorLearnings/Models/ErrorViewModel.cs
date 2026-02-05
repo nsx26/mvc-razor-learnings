6aabebbae275fa3c3ed89c4429ded39325f548b2
Nikita Sietsema <nikita.sietsema@xprize.org>
2026-02-05 13:39:29 -0500

    init mvc razor project

---
+++ McvRazorLearnings/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace McvRazorLearnings.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # mvc-razor-learnings
 This is an MVC Razor App Demonstrating recent learnings
+
+### Init
+Generated via ASP.NET Core Web App (Model-View-Controller) in VS.
