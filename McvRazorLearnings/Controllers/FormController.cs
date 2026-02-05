40f5ce6e52ec0b05e3ea7f5bc04ea5a629f0bd36
Nikita Sietsema <nikita.sietsema@xprize.org>
2026-02-05 17:18:33 -0500

    a working form, not annotated yet

---
+++ McvRazorLearnings/Controllers/FormController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcRazorLearnings.Models;
+
+namespace MvcRazorLearnings.Controllers;
+
+public class FormController : Controller
+{
+    public IActionResult Index()
+    {
+        return View(new FormInputModel());
+    }
+
+    [HttpPost]
+    public IActionResult Index(FormInputModel model)
+    {
+        if (!string.IsNullOrEmpty(model.InputText))
+        {
+            model.Result = model.InputText;
+        }
+        return View(model);
+    }
+}
---
+++ McvRazorLearnings/Models/FormInputModel.cs
@@ -0,0 +1,7 @@
+namespace MvcRazorLearnings.Models;
+
+public class FormInputModel
+{
+    public string InputText { get; set; } = string.Empty;
+    public string Result { get; set; } = string.Empty;
+}
